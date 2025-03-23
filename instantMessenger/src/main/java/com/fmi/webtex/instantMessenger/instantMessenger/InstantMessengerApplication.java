48fb23bc1dca6a01a8dfb221faf108955a43d1ce
NiaPopova <63757442+NiaPopova@users.noreply.github.com>
2025-03-23 12:47:00 +0200

    initial commit

---
+++ instantMessenger/src/main/java/com/fmi/webtex/instantMessenger/instantMessenger/InstantMessengerApplication.java
@@ -0,0 +1,13 @@
+package com.fmi.webtex.instantMessenger.instantMessenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstantMessengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstantMessengerApplication.class, args);
+	}
+
+}
---
+++ instantMessenger/src/test/java/com/fmi/webtex/instantMessenger/instantMessenger/InstantMessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmi.webtex.instantMessenger.instantMessenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstantMessengerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
