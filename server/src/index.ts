d9c6c795f351241d3fe0943b556d47bb6f02eb3c
Elena Karabeteva <e.karabeteva6@gmail.com>
2025-04-23 16:50:28 +0300

    Added db connection

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,19 @@
 import * as express from 'express';
+import mongoose from 'mongoose';
+import { Server } from 'socket.io';
+
+import messages from './routes/messages';
+import users from './routes/users';
+mongoose.connect('mongodb://localhost:27017/messages');
 
 const app = express();
+
+app.use('/messages', messages);
+app.use('/users', users);
+
 
 app.listen(4002, () => {
     console.log('Server is listening on port 4002')
 });
+
+
