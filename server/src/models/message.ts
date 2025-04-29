7b7cb82b48c5980fe105fcc500a70bb41f92ff5f
Elena Karabeteva <e.karabeteva6@gmail.com>
2025-04-29 22:32:17 +0300

    Modified message model in message.ts file

--- server/src/models/message.ts
+++ server/src/models/message.ts
@@ -1,9 +1,18 @@
 import mongoose from "mongoose";
 
-const messageSchema = new mongoose.Schema({
-    sender: String,
-    receiver: String,
-    content: String,
+const messageSchema: mongoose.Schema = new mongoose.Schema({
+    sender: {
+        type: String,
+        required: true
+    },
+    receiver: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
     timestamp: {
         type: Date,
         default: Date.now
