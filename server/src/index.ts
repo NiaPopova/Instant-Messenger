2e0cf090b6c2a84d2a591e3f707d7e888ae9a3d3
Elena Karabeteva <e.karabeteva6@gmail.com>
2025-04-29 22:30:00 +0300

    Modified index.ts file

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,19 +1,42 @@
 import * as express from 'express';
+import * as http from 'http';
 import mongoose from 'mongoose';
+import * as cors from 'cors';
+import * as dotenv from 'dotenv';
 import { Server } from 'socket.io';
+import messageRoutes from './routes/messages';
+import userRoutes from './routes/users';
+import authRoutes from './routes/auth';
+import { socketSetUp } from './socketio';
+import * as path from 'path';
 
-import messages from './routes/messages';
-import users from './routes/users';
-mongoose.connect('mongodb://localhost:27017/messages');
+dotenv.config();
+
+const options = {
+    cors: { 
+        origin: '*', 
+        methods: ['GET', 'POST'] 
+    }
+};
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server, options);
 
-app.use('/messages', messages);
-app.use('/users', users);
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '../../client/src')));
 
+app.use('/messages', messageRoutes);
+app.use('/users', userRoutes);
+app.use('/auth', authRoutes);
 
-app.listen(4002, () => {
-    console.log('Server is listening on port 4002')
-});
+socketSetUp(io);
 
+const url = `${process.env.DB_URL}/${process.env.DB_NAME}`;
 
+mongoose.connect(url)
+  .then(() => {
+    server.listen(process.env.SERVER_PORT, () => console.log(`Server on port ${process.env.SERVER_PORT}`));
+  })
+  .catch(err => console.error('Mongo error:', err));
