d60592f736680ee3fc7e29c675517a31b93902c4
Elena Karabeteva <e.karabeteva6@gmail.com>
2025-04-29 22:30:29 +0300

    Modified socketio.ts file

--- server/src/socketio.ts
+++ server/src/socketio.ts
@@ -1,14 +1,15 @@
 import { Socket, Server} from 'socket.io';
 import Message from './models/message';
 
-const onlineUsers = new Map<string, string>();
+const onlineUsersList = new Map<string, string>();
 
 export const socketSetUp = (io: Server) => {
     io.on('connection', (socket: Socket) => {
+        console.log('User connected');
 
-        socket.on('register', (userID: string) => {
-            onlineUsers.set(userID, socket.id);
-            console.log(`Registered ${userID}`);
+        socket.on('log', (user: string) => {
+            onlineUsersList.set(user, socket.id);
+            console.log(`${user} logged in the application`);
         });
 
         socket.on('message', async ({ sender, receiver, content}) => {
@@ -18,17 +19,17 @@ export const socketSetUp = (io: Server) => {
             socket.emit('sent_message', { receiver, content, timestamp: message.timestamp });
 
             //send in real time 
-            const receiverSocket = onlineUsers.get(receiver);
+            const receiverSocket = onlineUsersList.get(receiver);
             if (receiverSocket) {
                 io.to(receiverSocket).emit('message', { sender, content, timestamp: message.timestamp});
             }
         });
 
         socket.on('disconnect', () => {
-            onlineUsers.forEach((socketID, userID) => {
+            onlineUsersList.forEach((socketID, user) => {
                 if (socketID === socket.id) {
-                    onlineUsers.delete(userID);
-                    console.log(`${userID} disconnected`);
+                    onlineUsersList.delete(user);
+                    console.log(`${user} disconnected from the application`);
                 }
             })
         });
