25789e06543d30f9f3c57c205e1b8a61e3e6b497
NiaPopova <63757442+NiaPopova@users.noreply.github.com>
2025-06-04 22:20:35 +0300

    Update user.ts

--- models/user.ts
+++ models/user.ts
@@ -2,6 +2,7 @@ import mongoose, { Document, Schema } from 'mongoose';
 
 export interface IUser extends Document {
     name: string;
+    username: string;
     email: string;
     password: string;
     profile_pic: string;
@@ -10,10 +11,10 @@ export interface IUser extends Document {
 
 const UserSchema = new Schema<IUser>({
     name: { type: String, required: true },
-    username: { type: String, required: true},
+    username: { type: String, required: true },
     email: { type: String, required: true, match: /^\S+@\S+\.\S+$/ },
     password: { type: String, required: true },
-    profile_pic: { type: String, required: true },
+    profile_pic: { type: String, required: false },
     last_active: { type: Date, required: true }
 });
 
