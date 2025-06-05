2ce654eb7e44554d52f778dd725b0fa4f46db4c4
Elena Karabeteva <e.karabeteva6@gmail.com>
2025-06-05 20:20:05 +0300

    Added user route

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -1,5 +1,6 @@
 import * as express from 'express';
 import {User} from '../models/user';
+import { Channel } from '../models/channel';
 
 const users: express.Router = express.Router();
 
@@ -10,6 +11,26 @@ users.get('/', async (request: express.Request, response: express.Response) => {
     } catch (error) {
         response.status(500).json({ error: 'Error fetching users' });
     }
+});
+
+//get users from a channel
+users.get('/:channelId', async (req, res) => {
+  const { channelId } = req.params;
+
+  try {
+    const channel = await Channel.findById(channelId);
+    if (!channel) {
+      res.status(404).json({ message: 'Channel not found' });
+    }
+
+    // Get full user data based on IDs in user_list
+    const users = await User.find({ _id: { $in: channel?.user_list } }).select('name username email profile_pic');
+
+    res.status(200).json(users);
+  } catch (error) {
+    console.error('Error fetching users in channel:', error);
+    res.status(500).json({ message: 'Server error' });
+  }
 });
 
 export default users;
--- server/src/socketio.ts
+++ server/src/socketio.ts
@@ -1,5 +1,6 @@
 import { Socket, Server} from 'socket.io';
 import {Message} from '../src/models/mesage';
+import { Channel } from './models/channel';
 
 const onlineUsersList = new Map<string, string>();
 
@@ -12,18 +13,40 @@ export const socketSetUp = (io: Server) => {
             console.log(`${user} logged in the application`);
         });
 
-        socket.on('message', async ({ sender, receiver, content}) => {
-            const message = new Message( { sender, receiver, content, timestamp: new Date() });
-            await message.save();
+        socket.on('join-channel', ({ channelId }) => {
+            socket.join(channelId);
+            console.log(`Socket ${socket.id} joined channel ${channelId}`);
+        });
 
-            socket.emit('sent_message', { receiver, content, timestamp: message.timestamp });
 
-            //send in real time 
-            const receiverSocket = onlineUsersList.get(receiver);
-            if (receiverSocket) {
-                io.to(receiverSocket).emit('message', { sender, content, timestamp: message.timestamp});
+        socket.on('send-message', async ({ channelId, message }) => {
+            try {
+                const savedMessage = new Message(message);
+                await savedMessage.save();
+
+                await Channel.findByIdAndUpdate(channelId, {
+                    $push: { message_list: savedMessage._id }
+                });
+
+                io.to(channelId).emit('receive-message', savedMessage);
+
+            } catch (error) {
+                console.error('Error sending message:', error);
             }
         });
+
+        // socket.on('message', async ({ sender, receiver, content}) => {
+        //     const message = new Message( { sender, receiver, content, timestamp: new Date() });
+        //     await message.save();
+
+        //     socket.emit('sent_message', { receiver, content, timestamp: message.timestamp });
+
+        //     //send in real time 
+        //     const receiverSocket = onlineUsersList.get(receiver);
+        //     if (receiverSocket) {
+        //         io.to(receiverSocket).emit('message', { sender, content, timestamp: message.timestamp});
+        //     }
+        // });
 
         socket.on('disconnect', () => {
             onlineUsersList.forEach((socketID, user) => {
